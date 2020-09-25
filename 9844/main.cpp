e7fd110a722cff9225229f7c9d433fca148a0367
outerskyb <outerskyb@gmail.com>
2020-09-25 11:43:56 +0900

    9844

--- 9844/main.cpp
+++ 9844/main.cpp
@@ -1,10 +1,19 @@
 #include "bits/stdc++.h"
+#define max(a,b) (((a)>(b))?(a):(b))
 
 using namespace std;
+
+int map[500][500];
+int dp[500][500];
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+
+    for(int i = 0;  i < n ; i++){
+        for(int j = 0; j < n;j++){
+            
+        }
+    }
 }
