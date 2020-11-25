731664905eaa58c9454e515122fb115a5f5f0b09
outerskyb <outerskyb@gmail.com>
2020-11-25 15:21:49 +0900

    changed

---
+++ 16067/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    int m[100000];
+    for(int i =0;i<n;i++)
+    cin>>m[i];
+}
